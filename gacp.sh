e7e94912c28f9915cecad751e46acc6c402a699f
Nishani Kasineshan <e18245@eng.pdn.ac.lk>
2022-11-19 01:54:57 +0530

    updated script

--- gacp.sh
+++ gacp.sh
@@ -2,31 +2,43 @@
 #script to automatically git add ., git commit -m 'some commit message' and git push 
 
 echo "************Automating git add,commit and push************"
+echo "----------------------------------------------------------"
 
 addcommitpush(){
     #check git status
     git status
     #perform git add operation
     git add .
+    echo "------------------------------------------------------------------"
     echo "Files added to the staging area"
+    echo "------------------------------------------------------------------"
     #check git status
     git status
     #perform git commiit with a message(taken from command line argument)
-    message=\'"$@"\'
+    message="$@"
     git commit -m "$message"
+    echo "------------------------------------------------------------------"
     echo "Files in the staging area are snapshotted"
+    echo "------------------------------------------------------------------"
     #check git status
     git status
     #perform git push
-    git push 
+    git push
+    echo "------------------------------------------------------------------" 
     echo "Files pushed to github"
+    echo "------------------------------------------------------------------"
     #check git status
     git status
+    echo "------------------------------------------------------------------" 
+    echo "Github Automation Completed"
+    echo "------------------------------------------------------------------"
+   
 }
 
 usage(){
     echo "This is a simple script to perform git operations directly to main"
-    echo "bash gacp.sh 'some commit message'"
+    echo "Usage : bash gacp.sh 'some commit message'"
+    echo "------------------------------------------------------------------"
 }
 
 usage
